af7070477a63053646829f6cebaf54cea6e3e4e0
Daniele Di Modica <d.dimo94@hotmail.com>
2022-08-17 16:44:36 +0100

    Moving Platform
    The platform now moves back and forth.

--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,13 +15,32 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	StartLocation = GetActorLocation();
 }
 
 // Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
+	
 	Super::Tick(DeltaTime);
 
+	// Move platform forwards
+		// Get current location (local variable)
+	FVector CurrentLocation = GetActorLocation();
+		// Add vector to that location
+	CurrentLocation += PlatformVelocity * DeltaTime;
+		// Set the location
+	SetActorLocation(CurrentLocation);
+	// Send platform back if gone too far
+		// Check how far we've moved
+	float DistanceMoved = FVector::Distance(StartLocation, CurrentLocation);
+		// Reverse direction of motion if gone too far
+	if (DistanceMoved > MoveDistance)
+	{
+		PlatformVelocity = -PlatformVelocity;
+
+		StartLocation = CurrentLocation;
+	}
 }
 
--- ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
+++ ObstacleAssault/Source/ObstacleAssault/MovingPlatform.h
@@ -23,7 +23,11 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	UPROPERTY(EditAnywhere)
-	int32 MyInt = 99;
+	UPROPERTY(EditAnywhere, Category="Moving Platform")
+	FVector PlatformVelocity = FVector(100, 0, 0);
+	
+	UPROPERTY(EditAnywhere, Category="Moving Platform")
+	float MoveDistance;
 
+	FVector StartLocation;
 };
